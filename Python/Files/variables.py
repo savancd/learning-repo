c499b6ddff9b32d60cb99c0a5140ff9b5799e739
savancd <m.sava84@gmail.com>
2023-09-01 00:47:02 -0400

    Second update with new Python files

---
+++ Python/Files/variables.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+	
+def main():
+	book = "Dracula"
+	author = "Bram Stoker"
+	release_year = 1897
+	goodreads_rating = 4.01
+		
+	print(book)
+	print(author)
+	print(release_year)
+	print(goodreads_rating)
+	
+
+#    --	variable names in a single line using commas as separators --	
+	
+# def main():
+#    book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', 1897, 4.01
+
+ 
+
+
+		
+if __name__ == "__main__":
+	main()
