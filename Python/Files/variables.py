9299745995816803404259da31814bfc0490b4ce
savancd <m.sava84@gmail.com>
2023-09-02 21:46:17 -0400

    Update

--- Python/Files/variables.py
+++ Python/Files/variables.py
@@ -6,10 +6,10 @@ def main():
 	release_year = 1897
 	goodreads_rating = 4.01
 		
-	print(book)
-	print(author)
-	print(release_year)
-	print(goodreads_rating)
+#	print(book)
+#	print(author)
+#	print(release_year)
+#	print(goodreads_rating)
 	
 
 #    --	variable names in a single line using commas as separators --	
@@ -18,8 +18,13 @@ def main():
 #    book, author, release_year, goodreads_rating = 'Dracula', 'Bram Stoker', 1897, 4.01
 
  
+#	-- With the print() method, I can use + sign to add variables with strings inside a print method --
 
+#	print(book + " is a novel, from the " + author + ", published in " + str(release_year) + ". It has a rating of " + str(goodreads_rating) + " on goodreads. ")
 
+	
+	print(f"{book} is a novel written by {author}, published in {release_year}."
+		f" It has rating of {goodreads_rating} on goodreads." )
 		
 if __name__ == "__main__":
 	main()
