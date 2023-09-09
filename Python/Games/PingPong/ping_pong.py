58551708819cc4556b4e99b8d622723e19c94f6b
unknown <m.sava84@gmail.com>
2023-09-09 15:26:49 +0200

    update

--- Python/Games/PingPong/ping_pong.py
+++ Python/Games/PingPong/ping_pong.py
@@ -17,6 +17,12 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+
+score_a = 0
+score_b = 0
+
+
 
 # Paddle A
 
@@ -51,6 +57,15 @@ ball.dx = 0.1 # speed of the ball movenment
 ball.dy = -0.1 # speed of the ball movenment
 
 
+# Pen
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 18, "normal"))
 
 
 # Function
@@ -115,10 +130,17 @@ while True:
 	if ball.xcor() > 390:
 		ball.goto(0, 0)
 		ball.dx *= -1
+		score_a += 1
+		pen.clear()
+		pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 18, "normal"))
 
 	if ball.xcor() < -390:
 		ball.goto(0, 0)
 		ball.dx *= -1
+		score_b += 1
+		pen.clear()
+		pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 18, "normal"))
+
 
 # Paddle and ball collision
 	if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -54):
