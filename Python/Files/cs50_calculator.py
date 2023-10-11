81715c078070e774af595f6b35bc33f00d772108
savancd <m.sava84@gmail.com>
2023-10-11 19:35:00 -0400

    Update

---
+++ Python/Files/cs50_calculator.py
@@ -0,0 +1,13 @@
+# 	Concatinate 
+
+x = input("What's x? ")
+y = input("What's y? ")
+
+z = int(x) + int(y)	# converting to interger
+
+print(z)
+
+#	Converting to interger
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
