2c1497d450a8cc6e21ee58f8ca6b069303ebc23e
savancd <m.sava84@gmail.com>
2023-10-11 19:53:46 -0400

    Update

--- Python/Files/cs50_calculator.py
+++ Python/Files/cs50_calculator.py
@@ -11,3 +11,13 @@ print(z)
 
 x = int(input("What's x? "))
 y = int(input("What's y? "))
+
+#	Converting to floatclrea
+
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+
+#	str - string is sequence of text
+#	int - is a interger like 0 and 1
+#	float i a decimal point
