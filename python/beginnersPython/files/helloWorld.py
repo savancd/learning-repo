46c614093014ce368a9974cdf284b42c8e357715
savancd <m.sava84@gmail.com>
2024-04-08 20:27:35 -0400

    update

---
+++ python/beginnersPython/files/helloWorld.py
@@ -0,0 +1,18 @@
+#print("Hello World")
+
+#Greeting="Hello World"
+#name="Boby"
+#print(Greeting, name)
+
+
+#msg1="Hello"
+#msg2="Welcome to Python"
+
+#print(msg1, msg2)
+
+
+x=6
+y=2
+
+z=x+y
+print(z)
---
+++ python/beginnersPython/files/lists.py
@@ -0,0 +1,10 @@
+# Create a list of intergers
+my_list = [1, 2, 3, 4, 5, 6]
+
+# Create a list of mixed data types
+my_strings = ["Apple", "Banana", "Chery"]
+
+# Create a list of mixed data types
+mixed_list = [1, "hello", 3,22, True]
+
+print(my_list[1])
