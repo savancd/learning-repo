2194902a604b5764252d1c0df4f9f10390f10f85
unknown <m.sava84@gmail.com>
2024-04-30 02:47:06 +0200

    New progress on learning Pythong on Code in Place

---
+++ python/codeInPlace/python_course/how_old.py
@@ -0,0 +1,26 @@
+"""
+This is a worked example. This code is starter code; you should edit and run it to 
+solve the problem. You can click the blue show solution button on the left to see 
+the answer if you get too stuck or want to check your work!
+"""
+
+def main():
+    anthon = 21
+    beth = anthon + 6
+    chen = beth + 20
+    drew = chen + anthon
+    ethan = chen
+
+    print("Anthon is " + str(anthon))
+    print("Beth is " + str(beth))
+    print("Chen is " + str(chen))
+    print("Drew is " + str(drew))
+    print("Ethan is " + str(ethan))
+
+
+
+
+# There is no need to edit code beyond this point
+
+if __name__ == '__main__':
+    main()
