f4abee7c69dc2a4cdda3b7450781d12213698423
unknown <m.sava84@gmail.com>
2024-05-03 02:49:48 +0200

    Updating with new files following code in place course

--- python/codeInPlace/karel_course/midpoint.py
+++ python/codeInPlace/karel_course/midpoint.py
@@ -4,23 +4,36 @@ from karel.stanfordkarel import *
 This was optional challenge for the course that I did (Unsucessfully) I did no manage to make it work properly.
 I manage to solve the middle point in the 5x5 world but 6x5 world always was on wrong position placenment of Karel.
 When I manage to make it work on 6x5 world then is the issue with 5x5 world, so it doesn't work on every world 
-properly in the same time
+properly in the same time.
+
+
+Edit:
+
+At the few days later attempt I managed tp fond a solution to the problem of middle karel
+"""
+
+from karel.stanfordkarel import *
+
+"""
+File: main.py
+--------------------
+When you finish writing this file, Karel should be able to find
+the midpoint
 """
 
 def main():
    
     row_of_beepers()
     ends()
-    last_beeper()
+    last_beep()
 
-    # place last beeper
-def last_beeper():
-    while not_facing_east():
-        turn_left()
-    if facing_east():
-        move()
-    put_beeper()
+def last_beep():
+    turn_back()
     move()
+    put_beeper()
+    if not_facing_east():
+        turn_back()
+
 
 
     # remove end beepers
