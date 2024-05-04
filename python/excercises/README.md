e5ba4f449552fb1b9e8b0a80a3dff3808f5a2062
sava <m.sava84@gmail.com>
2024-05-04 15:38:20 +0200

    Updating with new files, learning python

---
+++ python/excercises/README.md
@@ -0,0 +1,4 @@
+#### List of excercises and my attempts to learn by doing without any tutorial
+
+1: Hello World
+2: Calculate the sum of two numbers
---
+++ python/excercises/two_num_sum.py
@@ -0,0 +1,13 @@
+# This is second succesfull attempt to make Python file without watching 
+# any tutorial or searching for any help online
+
+
+# Converting user input into float
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Result of the two user inputs
+result = num1 + num2
+
+# Printing the result and converting user input into string
+print("The result of given number is " + str(result))
